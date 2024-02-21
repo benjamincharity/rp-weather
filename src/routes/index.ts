3a0d139db4add2f0d311c4dc610ce48fbdf8a94e
Benjamin Charity <ben.charity@hey.com>
2024-02-20 21:20:06 -0500

    fix: redirect to forecast

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,11 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { Home } from "../pages/Home";
 
 export const Route = createFileRoute("/")({
   component: Home,
+  beforeLoad: async () => {
+    throw redirect({
+      to: "/forecast",
+    });
+  },
 });
